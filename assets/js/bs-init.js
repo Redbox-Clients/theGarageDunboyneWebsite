716f98b6b998a38cdad0d254af4d6736dea9d25d
Robert Coleman <robertcoleman@Roberts-MacBook-Pro.local>
2025-08-07 14:23:17 +0100

    initial commit

---
+++ assets/js/bs-init.js
@@ -0,0 +1,37 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+$(document).ready(function(){
+	AOS.init({ disable: 'mobile' });
+});
+
+}
+/*
+     FILE ARCHIVED ON 09:18:55 Mar 24, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 15:36:26 Jun 22, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 82.435
+  exclusion.robots: 0.119
+  exclusion.robots.policy: 0.111
+  cdx.remote: 0.056
+  esindex: 0.008
+  LoadShardBlock: 57.978 (3)
+  PetaboxLoader3.datanode: 120.76 (4)
+  load_resource: 125.366
+  PetaboxLoader3.resolve: 55.863
+*/
